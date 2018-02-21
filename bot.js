c56f479b64e173c3ce241260f3bdea52652a4933
JinGamerZ <34589116+JinGamerZ@users.noreply.github.com>
2018-02-21 14:26:42 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on("ready", () => {
-    client.user.setActivity({game: {name: "with my code", type: 0}});
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
