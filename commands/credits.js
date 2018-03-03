dc41eb44a9dc9b7e1f57dbfd6a1193d227f1feac
JinGamerZ <34589116+JinGamerZ@users.noreply.github.com>
2018-03-02 21:37:44 -0300

    Create credits.js

---
+++ commands/credits.js
@@ -0,0 +1,9 @@
+module.exports = {
+     main: function(bot, msg) {
+		var start = Date.now();
+		var stop, diff;
+		msg.channel.sendMessage("Wiim has been created by JinGamerZ.").then(function(newMsg) {
+		});
+    },
+    help: 'Creator.'
+};
