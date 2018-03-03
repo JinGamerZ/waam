76f90ec2ab39ea6a839e6c0c777d2972738de081
JinGamerZ <34589116+JinGamerZ@users.noreply.github.com>
2018-03-02 21:00:52 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,9 @@ var fs = require('fs');
 
 var bot = new Discord.Client({autoReconnect: true});
 
-bot.OWNERID = '<your-discord-id>';
-bot.PREFIX = '<bot-prefix>';
-bot.TOKEN = '<bot-token>';
+bot.OWNERID = '311679965420716043';
+bot.PREFIX = '-';
+bot.TOKEN = '<BOT TOKEN>';
 
 bot.DETAILED_LOGGING = false;
 bot.DELETE_COMMANDS = false;
