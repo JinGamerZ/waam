7a951398486481c91fbb9a725771fe61c4bd9aae
JinGamerZ <34589116+JinGamerZ@users.noreply.github.com>
2018-03-02 21:30:31 -0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -2,10 +2,7 @@ module.exports = {
      main: function(bot, msg) {
 		var start = Date.now();
 		var stop, diff;
-		msg.channel.sendMessage("Invite me!").then(function(newMsg) {
-			var stop = Date.now();
-			var diff = (stop - start);
-			newMsg.edit("Invite me! https://discordapp.com/api/oauth2/authorize?client_id=415709298048499714&permissions=8&scope=bot");
+		msg.channel.sendMessage("Invite me! https://discordapp.com/api/oauth2/authorize?client_id=415709298048499714&permissions=8&scope=bot").then(function(newMsg) {
 		});
     },
     help: 'Invitation Link.'
